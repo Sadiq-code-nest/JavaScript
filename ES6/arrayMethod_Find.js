e2cdecf263e88910b058d6c275f93ec3710dafe8
Sadiq <sadiqict63@gmail.com>
2025-05-04 17:56:17 +0600

    Added

---
+++ ES6/arrayMethod_Find.js
@@ -0,0 +1,4 @@
+// Find method -- find the first that we want
+let number= [11,33,31,20,36,21,39];
+let letsFind=number.find(x=>x%2==0);
+console.log(letsFind);
---
+++ ES6/arrayMethod_Find_Index.js
@@ -0,0 +1,5 @@
+// Find method -- tell the first things of index that we want
+
+let number= [11,33,31,20,36,21,39];
+let letsFind=number.findIndex(x=>x%2==0);
+console.log(letsFind);
