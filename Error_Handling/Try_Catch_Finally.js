3ae9217e893e1b0ecb04fd5ee4181c6b886b7f8d
Sadiq <sadiqict63@gmail.com>
2025-05-04 11:04:02 +0600

    Try Catch

--- Error_Handling/Try_Catch_Finally.js
+++ Error_Handling/Try_Catch_Finally.js
@@ -1,16 +1,19 @@
+//Try Catch Finally use
+
+
 try {
     //Test code
-alert(' Try Function');
-alert (x);
-alert(' Again Try Function');
-   
+    alert(' Try Function');
+    alert(x);
 } catch (error) {
     //Handle error
-
     alert('Inside Catch block');
 
     console.log('Catch')
-console.log(error);
+    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
 
-
+} finally {
+    alert(' switch back to Finally Function');
 }
