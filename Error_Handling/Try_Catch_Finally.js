498f6d869207657f401ce90ba78ab1fc6c6170c3
Sadiq <sadiqict63@gmail.com>
2025-05-04 10:54:30 +0600

    New

---
+++ Error_Handling/Try_Catch_Finally.js
@@ -0,0 +1,16 @@
+try {
+    //Test code
+alert(' Try Function');
+alert (x);
+alert(' Again Try Function');
+   
+} catch (error) {
+    //Handle error
+
+    alert('Inside Catch block');
+
+    console.log('Catch')
+console.log(error);
+
+
+}
