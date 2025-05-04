b7c4bcad3c0694bfc3ce6b6718d44785fc6c8c02
Sadiq <sadiqict63@gmail.com>
2025-05-04 11:20:08 +0600

    Let var const

---
+++ ES6/Let_Const_Var.js
@@ -0,0 +1,21 @@
+// Use of Var
+
+// Redeclare possible
+var x = 5;
+var x = 7;
+var x = 7;
+var x = 9;
+console.log(x);
+
+
+let y = 7;
+// let y=9; // can't Re-declared .
+y = 10;   // Only Can re-assign 
+console.log(y);
+
+
+
+const z = 22;
+// z=2; // Cant Redeclare and Re-assign. Assign only one time
+console.log(z);
+
